79a8cca55d350993cc70b7a56b9332d5ca8d6597
SENDE SOUMOUK Ben Fahad <yohannesende@gmail.com>
2026-06-08 19:12:34 +0200

    Ajouts des modifications et nouveaux fichiers

--- eden-hcr/backend/routes/adminRoutes.js
+++ eden-hcr/backend/routes/adminRoutes.js
@@ -4,8 +4,9 @@ import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-// On utilise le contrôleur getSuperAdminMetrics que tu as déjà défini
-// et qui contient toute ta logique (aggregate, countDocuments, etc.)
+// Si ton frontend appelle /api/admin/metrics
+// Dans server.js tu as : app.use('/api/admin', adminRoutes);
+// Donc ici, la route doit être juste '/metrics'
 router.get('/metrics', protect, getSuperAdminMetrics);
 
 export default router;
