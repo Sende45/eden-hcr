aff0e0f9241e146dda8d2c7ece027658f3f2143f
SENDE SOUMOUK Ben Fahad <yohannesende@gmail.com>
2026-06-28 20:27:28 +0200

    ajout des nouvelles modifications des fichiers

--- eden-hcr/backend/middlewares/authMiddleware.js
+++ eden-hcr/backend/middlewares/authMiddleware.js
@@ -9,17 +9,20 @@ export const protect = async (req, res, next) => {
       token = req.headers.authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      // On récupère le user (sans son mot de passe) et on l'injecte dans la requête
       req.user = await User.findById(decoded.id).select('-password');
       return next();
     } catch (error) {
-      res.status(401);
-      return next(new Error('Non autorisé, jeton d’authentification invalide.'));
+      return res.status(401).json({
+        status: 'error',
+        message: 'Non autorise, jeton invalide.'
+      });
     }
   }
 
   if (!token) {
-    res.status(401);
-    return next(new Error('Accès refusé, aucun jeton d’authentification trouvé.'));
+    return res.status(401).json({
+      status: 'error',
+      message: 'Acces refuse, aucun jeton trouve.'
+    });
   }
 };
